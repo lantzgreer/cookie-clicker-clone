5368e1f867fdce3018c742346c14aac133d3609f
Marcus <xermedia@gmail.com>
2022-01-01 15:06:50 -0600

    changes and background image

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
 let rate = 1;
-let autoRateChange = 5;
+let autoRateChange = 0;
+
+// Tier 1 Button Stats
+let tier1RateThreshold = 10;
+let tier1RateCost = 10;
+let tier1RateIncrease = 5;
 
 document.getElementById("clickme").onclick = function () {
-    clickFunction();
-    };
-    function clickFunction() {
-    boxValue = Number(document.getElementById("counter").innerHTML);
-    boxValue = boxValue + rate;
-    document.getElementById("counter").innerHTML = boxValue;
-    
-    console.log(boxValue);
-    }
+  clickFunction();
+};
+function clickFunction() {
+  boxValue = Number(document.getElementById("counter").innerHTML);
+  boxValue = boxValue + rate;
+  document.getElementById("counter").innerHTML = boxValue;
+  autoStartCheck(boxValue);
+  console.log(boxValue);
+}
 
-    myInterval = setInterval(startTime, 1000);
+myInterval = setInterval(startTime, 1000);
 
-    function startTime() {
-        boxValue = Number(document.getElementById("counter").innerHTML);
-        boxValue = boxValue + autoRateChange;
-        document.getElementById("counter").innerHTML = boxValue;
+function startTime() {
+  boxValue = Number(document.getElementById("counter").innerHTML);
+  console.log(boxValue);
+  boxValue = boxValue + autoRateChange;
+  document.getElementById("counter").innerHTML = boxValue;
+  // Added this to show or disable
+  if (boxValue < tier1RateThreshold) {
+    document.getElementById("tier1button").disabled = true;
+  }
+  if (boxValue > tier1RateThreshold) {
+    document.getElementById("tier1button").disabled = false;
+  }
+}
 
-    }
+// Check to see when clicked if threshold = boxvalue
+function autoStartCheck(val) {
+  if (val == tier1RateThreshold) {
+    document.getElementById("tier1button").disabled = false;
+  }
+}
 
-    document.getElementById("reset").onclick = reset;
+document.getElementById("stop").onclick = function () {
+  clearInterval(myInterval);
+};
 
-    function reset() {
-        console.log('test');
-        clearInterval(myInterval);
-    }
+document.getElementById("reset").onclick = reset;
+
+function reset() {
+  document.getElementById("counter").innerHTML = 0;
+  document.getElementById("tier1button").disabled = true;
+
+  if (myInterval) {
+    clearInterval(myInterval);
+  }
+  location.reload();
+}
