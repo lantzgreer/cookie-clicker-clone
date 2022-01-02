be63f31b2e02406d143d817c6094ae0353f1b4cc
Marcus <xermedia@gmail.com>
2022-01-01 19:29:19 -0600

    updates to cps

--- index.js
+++ index.js
@@ -3,27 +3,30 @@ let autoRateChange = 0;
 
 // Tier 1 Button Stats
 let tier1RateThreshold = 10;
-let tier1RateCost = 10;
-let tier1RateIncrease = 5;
+//let tier1RateCost = 10;
+let tier1RateMultiplier = 2; // multiply times 2
+let tier1RateIncrease = 0.1;
 
 document.getElementById("clickme").onclick = function () {
   clickFunction();
 };
 function clickFunction() {
-  boxValue = Number(document.getElementById("counter").innerHTML);
+  boxValue = Number(document.getElementById("counter2").innerHTML);
   boxValue = boxValue + rate;
-  document.getElementById("counter").innerHTML = boxValue;
+  document.getElementById("counter").innerHTML = Math.round(boxValue);
+  document.getElementById("counter2").innerHTML = boxValue;
   autoStartCheck(boxValue);
-  console.log(boxValue);
+  console.log(Math.round(boxValue * 10) / 10);
 }
 
 myInterval = setInterval(startTime, 1000);
 
 function startTime() {
-  boxValue = Number(document.getElementById("counter").innerHTML);
-  console.log(boxValue);
+  boxValue = Number(document.getElementById("counter2").innerHTML);
+  console.log(Math.round(boxValue * 10) / 10);
   boxValue = boxValue + autoRateChange;
-  document.getElementById("counter").innerHTML = boxValue;
+  document.getElementById("counter").innerHTML = Math.round(boxValue);
+  document.getElementById("counter2").innerHTML = boxValue;
   // Added this to show or disable
   if (boxValue < tier1RateThreshold) {
     document.getElementById("tier1button").disabled = true;
@@ -40,6 +43,23 @@ function autoStartCheck(val) {
   }
 }
 
+document.getElementById("tier1button").onclick = function () {
+  // added so can only click once.
+  document.getElementById("tier1button").disabled = true;
+  //console.log("tett");
+  //beginTime();
+  autoRateChange = autoRateChange + tier1RateIncrease;
+
+  document.getElementById("rateAdded").innerHTML =
+    Math.round(autoRateChange * 100) / 100;
+  document.getElementById("counter").innerHTML = Math.round(
+    boxValue - tier1RateThreshold
+  );
+  document.getElementById("counter2").innerHTML = boxValue - tier1RateThreshold;
+  tier1RateThreshold = tier1RateThreshold * tier1RateMultiplier;
+  document.getElementById("tier1cost").innerHTML = tier1RateThreshold;
+};
+
 document.getElementById("stop").onclick = function () {
   clearInterval(myInterval);
 };
@@ -48,6 +68,7 @@ document.getElementById("reset").onclick = reset;
 
 function reset() {
   document.getElementById("counter").innerHTML = 0;
+  document.getElementById("counter2").innerHTML = 0;
   document.getElementById("tier1button").disabled = true;
 
   if (myInterval) {
